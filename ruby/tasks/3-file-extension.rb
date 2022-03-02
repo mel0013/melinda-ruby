21141f78ddf96267c8345f2e1234d497598b5a84
Melinda Batang <meeelinda.b@gmail.com>
2022-03-03 10:05:00 +1100

    completed task 3

--- ruby/tasks/3-file-extension.rb
+++ ruby/tasks/3-file-extension.rb
@@ -6,5 +6,12 @@
  # @returns {String} The file extension (with no period), or false if there is none.
 #
 def get_file_extension(filename)
-
+  filename.match(/\./).nil? ? false : filename.split('.').last
 end
+
+# check if there is a .file_extension using regex
+# extensions end in . so split from there and get the last element
+
+# Other method:
+# file_extension = File.extname(filename)
+# File#extname can't recognise .htaccess
